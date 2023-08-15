b68af6a7c79a17085a1eebd3ca5517f8d09b304a
Brahim Berkasse <berkassebrahim@gmail.com>
2023-08-15 18:29:11 +0100

    sidebar elements !

--- web/src/components/Sidebar.tsx
+++ web/src/components/Sidebar.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Box, Text, Center } from '@chakra-ui/react';
 import { BsChatLeftDots } from 'react-icons/bs'
 import { IoGameControllerOutline } from 'react-icons/io5'
-import { FiUsers, FiSettings } from 'react-icons/fi';
+import { FiUsers, FiSettings, FiLogOut } from 'react-icons/fi';
+import { MdOutlineLeaderboard } from 'react-icons/md'
 
 const _items = [
 	{
@@ -14,12 +15,20 @@ const _items = [
 		Icon: IoGameControllerOutline
 	},
 	{
+		name: "board",
+		Icon: MdOutlineLeaderboard
+	},
+	{
 		name: "friends",
 		Icon: FiUsers
 	},
 	{
 		name: "Settings",
 		Icon: FiSettings
+	},
+	{
+		name: "Logout",
+		Icon: FiLogOut
 	}
 ]
 
@@ -29,8 +38,8 @@ export const SideBar : React.FC = () => {
 		<Box display={'flex'} flexDir={'column'} alignItems={'center'}>
 			{
 				_items.map(({name, Icon}, key) => (
-					<Box textAlign={'center'} key={key} mb={'20px'}>
-						<Center h={'45px'} w={'45px'} rounded={'16px'} bg={'black'}>
+					<Box textAlign={'center'} key={key} mb={'20px'} transition={'.3s'} cursor={'pointer'} _hover={{transform: 'scale(.95)', transition: '.3s'}}>
+						<Center h={'45px'} w={'45px'} rounded={'16px'} bg={'black'} m={'auto'}>
 							<Icon color={'white'} size={'15px'} />	
 						</Center>
 						<Text fontWeight={'bold'} fontSize={'13px'}>{name}</Text>
