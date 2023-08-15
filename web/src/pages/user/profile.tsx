7fd23c27b86fbe40cb95e79018600364b9c35c30
Brahim Berkasse <berkassebrahim@gmail.com>
2023-08-15 16:24:58 +0100

    add badge section !

--- web/src/pages/user/profile.tsx
+++ web/src/pages/user/profile.tsx
@@ -1,16 +1,37 @@
-import { Box, Container } from '@chakra-ui/react';
+import { Box, Container, Text } from '@chakra-ui/react';
 import { PersonalInfo, Stats } from '../../components'; 
 
 export default function Profile(){
-
-
-
 
 	return (
 		<Box p={'40px'}>
 			<Container maxW='container.md' >
 				<PersonalInfo />
+				<Box mt={'30px'}>
+					<Text mb={'20px'} fontWeight={'bold'} fontSize={'30px'}>Badges</Text>
+					<Box overflow={'auto'} whiteSpace={'nowrap'}>
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+						<Box display={'inline-block'} h={'150px'} w={'200px'}  bg={'gray.100'} mr={'10px'} rounded={'18px'} />
+					</Box>
+				</Box>
 				<Stats />
+				
 			</Container>
 		</Box>
 	)
