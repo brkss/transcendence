5950b85b84e00053ec0762441cc2190a8f31c32b
houdaharit <houdaharit20@gmail.com>
2023-12-11 20:51:30 +0100

    added GameModule

--- server/src/user/user.module.ts
+++ server/src/user/user.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RoomService } from 'src/chat/room/room.service';
+import { GameService } from 'src/game/game.service';
 
 @Module({
-  providers: [UserService, PrismaService, RoomService],
+  providers: [UserService, PrismaService, RoomService, GameService],
   exports: [UserService]
 })
 export class UserModule {}
