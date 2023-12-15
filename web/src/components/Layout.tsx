7c4b82564c722eb806d647df429be2308329bcc4
houdaharit <houdaharit20@gmail.com>
2023-12-15 03:19:36 +0100

    frontend game

--- web/src/components/Layout.tsx
+++ web/src/components/Layout.tsx
@@ -1,106 +1,104 @@
-import React from "react";
-import { useRouter } from "next/router";
-import {
-  Box,
-  Grid,
-  Container,
-  GridItem,
-  useDisclosure,
-} from "@chakra-ui/react";
-import { TopBar } from "./TopBar";
-import { SideBar } from "./Sidebar";
+import React from 'react';
+import { useRouter } from 'next/router'
+import { Box, Grid, Container, GridItem, useDisclosure } from '@chakra-ui/react';
+import { TopBar } from './TopBar';
+import { SideBar } from './Sidebar';
 
-// Drawers ---
-import { FriendsDrawer } from "./FriendsDrawer";
-import { ChatDrawer } from "./ChatDrawer";
-import { SettingsDrawer } from "./SettingsDrawer";
+// Drawers --- 
+import { FriendsDrawer } from './FriendsDrawer';
+import { ChatDrawer } from './ChatDrawer';
+import { SettingsDrawer } from './SettingsDrawer';
+
+// private chat --
+import { PrivateChat } from './ChatDrawer/DM/Chat';
+import { setAccessToken } from '@/utils/token';
 
 const _singnals = [
-  {
-    name: "friends",
-  },
-  {
-    name: "chat",
-  },
-  {
-    name: "board",
-  },
-  {
-    name: "game",
-  },
-  {
-    name: "settings",
-  },
-  {
-    name: "logout",
-  },
-];
+	{
+		name: "friends"
+	},
+	{
+		name: "chat",
+	},
+	{
+		name: "board",
+	},
+	{
+		name: "game",
+	},
+	{
+		name: "settings",
+	},
+	{
+		name: "logout"
+	}
+]
+
 
 export const Layout: React.FC<any> = ({ children, disablePadding }) => {
-  const router = useRouter();
-  const showFriends = useDisclosure();
-  const showGame = useDisclosure();
-  const showBoard = useDisclosure();
-  const showSettings = useDisclosure();
-  const showChat = useDisclosure();
 
-  const handleSig = (sig: string) => {
-    console.log("handle sig : ", sig);
-    switch (sig) {
-      case "friends":
-        showFriends.onOpen();
-        break;
-      case "normal game":
-        router.push("/game");
-        break;
-      case "arcade game":
-        router.push("/game?arcade=on");
-        break;
-      case "settings":
-        showSettings.onOpen();
-        break;
-      case "chat":
-        showChat.onOpen();
-        break;
-      case "board":
-        router.push("/leaderboard");
-        break;
-      default:
-        break;
-    }
-  };
 
-  return (
-    <Box>
-      <TopBar />
-      {/* <Grid templateColumns={'repeat(12, 1fr)'}> */}
+	const router = useRouter();
+	const showFriends = useDisclosure();
+	const showGame = useDisclosure();
+	const showBoard = useDisclosure();
+	const showSettings = useDisclosure();
+	const showChat = useDisclosure();
 
-      <SideBar signal={(sig: string) => handleSig(sig)} />
+	const privateChatDrawer = useDisclosure();
+	const [chatUID, setChatUID] = React.useState<number>(-1);
 
-      {/* <GridItem colSpan={{ md: 12, base: 12 }} p={{ md: '40px', base: '15px' }} sx={{ paddingLeft: '90px' }}> */}
-      <div
-        style={{
-          padding: "10px",
-          paddingLeft: disablePadding ? "0px" : "95px",
-        }}
-      >
-        {children}
-      </div>
-      {/* </GridItem> */}
-      {/* </Grid> */}
-      {
-        <FriendsDrawer
-          isOpen={showFriends.isOpen}
-          onClose={showFriends.onClose}
-        />
-      }
-      {<ChatDrawer isOpen={showChat.isOpen} onClose={showChat.onClose} />}
-      {
-        <SettingsDrawer
-          isOpen={showSettings.isOpen}
-          onClose={showSettings.onClose}
-        />
-      }
-    </Box>
-  );
-};
+	const handleSig = (sig: string) => {
+
+		console.log("handle sig : ", sig);
+		switch (sig) {
+			case "friends":
+				showFriends.onOpen()
+				break;
+			case "game":
+				router.push("/game")
+				break;
+			case "settings":
+				showSettings.onOpen()
+				break;
+			case "chat":
+				showChat.onOpen()
+				break;
+			case "board":
+				router.push("/leaderboard")
+				break;
+			case "logout":
+				console.log("log out !")
+				setAccessToken("");
+				router.push('/auth/login')
+			default:
+				break;
+		}
+	}
+
+	const handlePrivateMessageChat = (userID: number) => {
+		setChatUID(userID);
+		privateChatDrawer.onOpen();
+	}
+
+	return (
+		<Box>
+			<TopBar />
+			{/* <Grid templateColumns={'repeat(12, 1fr)'}> */}
+
+			<SideBar signal={(sig: string) => handleSig(sig)} />
+
+			{/* <GridItem colSpan={{ md: 12, base: 12 }} p={{ md: '40px', base: '15px' }} sx={{ paddingLeft: '90px' }}> */}
+			<div style={{ padding: '10px', paddingLeft: disablePadding ? '0px' : '95px' }}>
+				{children}
+			</div>
+			{/* </GridItem> */}
+			{/* </Grid> */}
+			{<FriendsDrawer sendMessage={(uid: number) => handlePrivateMessageChat(uid)} isOpen={showFriends.isOpen} onClose={showFriends.onClose} />}
+			{<ChatDrawer isOpen={showChat.isOpen} onClose={showChat.onClose} />}
+			{<SettingsDrawer isOpen={showSettings.isOpen} onClose={showSettings.onClose} />}
+			
+			{<PrivateChat onClose={privateChatDrawer.onClose} isOpen={privateChatDrawer.isOpen} userId={chatUID} />}
+		</Box>
+	)
+}
