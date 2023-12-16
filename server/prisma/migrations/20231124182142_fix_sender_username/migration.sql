1f67dadd5e912e53d49feb8b30cccbe8ed729322
Brahim Berkasse <berkassebrahim@gmail.com>
2023-12-16 01:36:45 +0100

    merging : add members to private rooms !

--- server/prisma/migrations/20231124182142_fix_sender_username/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `sender_username` on the `Messages` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Messages" DROP COLUMN "sender_username";
