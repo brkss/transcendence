e8561beab916b966708c2e07f37867b66b625211
houdaharit <houdaharit20@gmail.com>
2023-12-17 05:43:30 +0100

    create game and add score

--- web/src/utils/services/game.axios.ts
+++ web/src/utils/services/game.axios.ts
@@ -3,6 +3,12 @@ import { api } from './axios.config';
 import { CreateGameInput, AddPlayerScoreInput } from '../types';
 
 
-export const createGame = async () => {
-const response = await api.post('${API_URL}/game/create');
+export const createGame = async (input: CreateGameInput) => {
+    const response = await api.post('${API_URL}/game/create');
+}
+
+export const addScore = async (firstPlayer_score: AddPlayerScoreInput, secondPlayer_score: AddPlayerScoreInput) => {
+    const response = await api.post('${API_URL}/game/addScore', firstPlayer_score, secondPlayer_score, {
+        headers: { 'Content-Type': 'application/json' }
+    });
 }
