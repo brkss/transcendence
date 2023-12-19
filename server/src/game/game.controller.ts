4f65f1046f23c1609319d3ac16f1fb33c8593ff6
houdaharit <houdaharit20@gmail.com>
2023-12-19 03:47:02 +0100

    added await

--- server/src/game/game.controller.ts
+++ server/src/game/game.controller.ts
@@ -9,23 +9,23 @@ export class GameController {
 
 	@Get()
 	async getAllGames(): Promise<any> {
-		return this.gameService.getAllGames();
+		return await this.gameService.getAllGames();
 	}
 
 	@Get(':id')
 	async getGame(@Param('id') id: number): Promise<any> {
-		return this.gameService.getGame(id);
+		return await this.gameService.getGame(id);
 	}
 
 	@Post("create")
 	async CreateGame(@Body() createGameDTO: CreateGameDTO): Promise<any> {
-		return this.gameService.createGame(createGameDTO);
+		return await this.gameService.createGame(createGameDTO);
 	}
 
 	@Post("addscore")
 	async addPlayerScores(@Body() firstPlayer_score: AddPlayerScoreDTO, secondPlayer_score: AddPlayerScoreDTO): Promise<any> {
-		const firstScore = this.gameService.addScore(firstPlayer_score.game_id, firstPlayer_score);
-		const secondScore = this.gameService.addScore(secondPlayer_score.game_id, secondPlayer_score);
+		const firstScore = await this.gameService.addScore(firstPlayer_score.game_id, firstPlayer_score);
+		const secondScore = await this.gameService.addScore(secondPlayer_score.game_id, secondPlayer_score);
 		return [firstScore , secondScore];
 	}
 }
