5fca98306ecbe32e9c56298a14cc47b045d0cf96
Driouch Ayoub <youba@adcover.local>
2023-12-22 02:11:53 +0100

    changin env file

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL : string = "http://localhost:8000";
-export const ALLOW_ORIGIN : string = "http://localhost:3000"
+export const API_URL : string = "http://localhost/api"
+export const ALLOW_ORIGIN : string = "http://localhost"
